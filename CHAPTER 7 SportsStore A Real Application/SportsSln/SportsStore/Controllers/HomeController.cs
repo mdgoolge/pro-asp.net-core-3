803769370d2935e352138dc08991de1a6a1032ed
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-03-23 13:20:43 +0800

    Updating the View

--- CHAPTER 7 SportsStore A Real Application/SportsSln/SportsStore/Controllers/HomeController.cs
+++ CHAPTER 7 SportsStore A Real Application/SportsSln/SportsStore/Controllers/HomeController.cs
@@ -3,14 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using SportsStore.Models;
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private IStoreRepository repository;
+        public HomeController(IStoreRepository repo)
         {
-            return View();
+            repository = repo;
         }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public IActionResult Index() => View(repository.Products);
     }
 }

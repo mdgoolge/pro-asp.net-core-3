f4e7f09a3d055a464aa8a580907506458162f7f7
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-03-23 14:33:17 +0800

    PAGINATION

--- CHAPTER 7 SportsStore A Real Application/SportsSln/SportsStore/Controllers/HomeController.cs
+++ CHAPTER 7 SportsStore A Real Application/SportsSln/SportsStore/Controllers/HomeController.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SportsStore.Models;
+using System.Linq;
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
-        private IStoreRepository repository;
+        private IStoreRepository repository; 
+        public int PageSize = 4;
         public HomeController(IStoreRepository repo)
         {
             repository = repo;
@@ -17,6 +19,11 @@ namespace SportsStore.Controllers
         //{
         //    return View();
         //}
-        public IActionResult Index() => View(repository.Products);
+        //public IActionResult Index() => View(repository.Products);
+        public ViewResult Index(int productPage = 1)
+                                => View(repository.Products
+                                .OrderBy(p => p.ProductID)
+                                .Skip((productPage - 1) * PageSize)
+                                .Take(PageSize));
     }
 }

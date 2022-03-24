6725a918b58d8e40882ac3176ca94a1dd8cd7892
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-03-24 10:21:46 +0800

    CHAPTER 8 SportsStore Navigation and Cart

---
+++ CHAPTER 8 SportsStore Navigation and Cart/SportsSln/SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository; 
+        public int PageSize = 4;
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        //public IActionResult Index() => View(repository.Products);
+        //public ViewResult Index(int productPage = 1)
+        //                        => View(repository.Products
+        //                        .OrderBy(p => p.ProductID)
+        //                        .Skip((productPage - 1) * PageSize)
+        //                        .Take(PageSize));
+        public ViewResult Index(int productPage = 1)
+                            => View(new ProductsListViewModel
+                            {
+                                Products = repository.Products
+                            .OrderBy(p => p.ProductID)
+                            .Skip((productPage - 1) * PageSize)
+                            .Take(PageSize),
+                                PagingInfo = new PagingInfo
+                                {
+                                    CurrentPage = productPage,
+                                    ItemsPerPage = PageSize,
+                                    TotalItems = repository.Products.Count()
+                                }
+                            });
+
+    }
+}

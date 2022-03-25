cfa53690d7eb9eec7ba7b04c0951ba3fb8f7f549
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-03-25 16:32:03 +0800

    Preparing Blazor

---
+++ CHAPTER 10 SportsStore Administration/SportsSln/SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+        public int PageSize = 4;
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        //public IActionResult Index() => View(repository.Products);
+        //public ViewResult Index(int productPage = 1)
+        //                        => View(repository.Products
+        //                        .OrderBy(p => p.ProductID)
+        //                        .Skip((productPage - 1) * PageSize)
+        //                        .Take(PageSize));
+        public ViewResult Index(string category, int productPage = 1)
+                            => View(new ProductsListViewModel
+                            {
+                                Products = repository.Products
+                                .Where(p => category == null || p.Category == category)
+                                .OrderBy(p => p.ProductID)
+                                .Skip((productPage - 1) * PageSize)
+                                .Take(PageSize),
+                                PagingInfo = new PagingInfo
+                                {
+                                    CurrentPage = productPage,
+                                    ItemsPerPage = PageSize,
+                                    //TotalItems=repository.Products.Count()
+                                    TotalItems = category == null ?
+                                        repository.Products.Count() :
+                                        repository.Products.Where(e =>
+                                            e.Category == category).Count()
+                                },
+                                CurrentCategory = category
+                            });
+
+    }
+}

d8a3a7c8da4c70d4d67164259a637a70f46362b3
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-03-29 15:14:47 +0800

    Configuring Error Handling

--- CHAPTER 11 SportsStore Security and Deployment/SportsSln/SportsStore/Startup.cs
+++ CHAPTER 11 SportsStore Security and Deployment/SportsSln/SportsStore/Startup.cs
@@ -50,12 +50,21 @@ namespace SportsStore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            app.UseDeveloperExceptionPage();
-            app.UseStatusCodePages();
+            ////if (env.IsDevelopment())
+            ////{
+            ////    app.UseDeveloperExceptionPage();
+            ////}
+            //app.UseDeveloperExceptionPage();
+            //app.UseStatusCodePages();
+            if (env.IsProduction())
+            {
+                app.UseExceptionHandler("/error");
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
             app.UseStaticFiles();
 
             app.UseSession();

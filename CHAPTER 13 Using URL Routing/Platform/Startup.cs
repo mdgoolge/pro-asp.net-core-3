f7b5754bdcfd01b087052834d093c5c837091546
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-03-31 16:50:05 +0800

    Preparing for This Chapter

---
+++ CHAPTER 13 Using URL Routing/Platform/Startup.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace Platform
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<MessageOptions>(Options =>
+            {
+                Options.CityName = "Albany";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+        //        IOptions<MessageOptions>msgOptions)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseMiddleware<Population>();
+            app.UseMiddleware<Capital>();
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Terminal middleware Reached");
+            });
+
+        }
+    }
+}

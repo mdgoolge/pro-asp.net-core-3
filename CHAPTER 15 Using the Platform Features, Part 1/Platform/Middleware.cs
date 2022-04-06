037da428927da6593d683b6cf4fc11e6588cf733
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-04-06 10:43:44 +0800

    Preparing for This Chapter

---
+++ CHAPTER 15 Using the Platform Features, Part 1/Platform/Middleware.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+namespace Platform
+{
+    public class QueryStringMiddleWare
+    {
+        private RequestDelegate next;
+        public QueryStringMiddleWare(RequestDelegate nextDelegate)
+        {
+            next = nextDelegate;
+        }
+        public QueryStringMiddleWare()
+        {
+
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Method == HttpMethods.Get
+            && context.Request.Query["custom"] == "true")
+            {
+                await context.Response.WriteAsync("Class-based Middleware \n");
+            }
+            //await next(context);
+            if (next != null)
+            {
+                await next(context);
+            }
+        }
+    }
+    public class LocationMiddleware
+    {
+        private RequestDelegate next;
+        private MessageOptions options;
+        public LocationMiddleware(RequestDelegate nextDelegate,
+        IOptions<MessageOptions> opts)
+        {
+            next = nextDelegate;
+            options = opts.Value;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path == "/location")
+            {
+                await context.Response
+                .WriteAsync($"{options.CityName}, {options.CountryName}");
+            }
+            else
+            {
+                await next(context);
+            }
+        }
+    }
+}

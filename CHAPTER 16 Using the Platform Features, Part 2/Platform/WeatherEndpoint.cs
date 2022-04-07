ce71fc6110fbe4ab748ff00f9d382bd0881ac3d9
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-04-07 10:50:39 +0800

    Preparing for This Chapter

---
+++ CHAPTER 16 Using the Platform Features, Part 2/Platform/WeatherEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Platform.Services;
+namespace Platform
+{
+    public class WeatherEndpoint
+    {
+        //private IResponseFormatter formatter;
+        //public WeatherEndpoint(IResponseFormatter responseFormatter)
+        //{
+        //    formatter = responseFormatter;
+        //}
+        public  async Task Endpoint(HttpContext context, IResponseFormatter formatter )
+        {
+            await formatter.Format(context, "Endpoint Class: It is cloudy in Milan");
+        }
+    }
+}

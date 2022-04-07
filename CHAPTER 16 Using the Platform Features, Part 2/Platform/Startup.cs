3889c9b25db1252fa863110cc8d71e9c4f5a8f83
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-04-07 14:48:06 +0800

    Using Session Data

--- CHAPTER 16 Using the Platform Features, Part 2/Platform/Startup.cs
+++ CHAPTER 16 Using the Platform Features, Part 2/Platform/Startup.cs
@@ -32,6 +32,16 @@ namespace Platform
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapGet("/cookie", async context => {
+                    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
+                    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
+                    context.Session.SetInt32("counter1", counter1);
+                    context.Session.SetInt32("counter2", counter2);
+                    await context.Session.CommitAsync();
+                    await context.Response
+                    .WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
+                });
+
                 endpoints.MapFallback(async context =>
                 await context.Response.WriteAsync("Hello World!"));
             });

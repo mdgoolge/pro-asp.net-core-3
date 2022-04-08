607f9fb9e17d97af80a7347f201cd5be84b09a2f
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-04-08 14:12:33 +0800

    Handling Exceptions and Errors

--- CHAPTER 16 Using the Platform Features, Part 2/Platform/Startup.cs
+++ CHAPTER 16 Using the Platform Features, Part 2/Platform/Startup.cs
@@ -32,7 +32,11 @@ namespace Platform
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
-            if (env.IsProduction())
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
             {
                 app.UseHsts();
             }
@@ -42,27 +46,8 @@ namespace Platform
             app.UseSession();
             app.UseRouting();
 
-            app.Use(async (context, next) =>
-            {
-                await context.Response
-                    .WriteAsync($"HTTPS Request:{context.Request.IsHttps} \n");
-                await next();
-            });
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/cookie", async context => {
-                    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
-                    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
-                    context.Session.SetInt32("counter1", counter1);
-                    context.Session.SetInt32("counter2", counter2);
-                    await context.Session.CommitAsync();
-                    await context.Response
-                    .WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
-                });
-
-                endpoints.MapFallback(async context =>
-                await context.Response.WriteAsync("Hello World!"));
+            app.Run(context => {
+                throw new Exception("Something has gone wrong");
             });
         }
     }

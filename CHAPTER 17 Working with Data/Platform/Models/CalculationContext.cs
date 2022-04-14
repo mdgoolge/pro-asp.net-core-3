c2ee6ba76c2c2d62db08cb5b6d6381ac6d9506c5
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-04-14 13:48:11 +0800

    Using Entity Framework Core Installing Entity Framework Core Creating the Data Model

---
+++ CHAPTER 17 Working with Data/Platform/Models/CalculationContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace Platform.Models
+{
+    public class CalculationContext : DbContext
+    {
+        public CalculationContext(DbContextOptions<CalculationContext> opts)
+        : base(opts) { }
+        public DbSet<Calculaton> Calculations { get; set; }
+    }
+}

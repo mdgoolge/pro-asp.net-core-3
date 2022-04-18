0b1611cbebc9779b36b36d6333d044b3176b4e24
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-04-18 14:35:06 +0800

    Understanding the Base Class Understanding the Controller Attributes Understanding Action Method Results Using Dependency Injection in Controllers

--- CHAPTER 19 Creating RESTful Web Services/WebApp/Controllers/ProductsController.cs
+++ CHAPTER 19 Creating RESTful Web Services/WebApp/Controllers/ProductsController.cs
@@ -1,27 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using WebApp.Models;
+using Microsoft.Extensions.Logging;
+using System.Linq;
 namespace WebApp.Controllers
 {
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        private DataContext context;
+        public ProductsController(DataContext ctx)
+        {
+            context = ctx;
+        }
+
+
         [HttpGet]
         public IEnumerable<Product> GetProducts()
         {
-            return new Product[] {
-new Product() { Name = "Product #1" },
-new Product() { Name = "Product #2" },
-};
+            return context.Products;
         }
+
+
         [HttpGet("{id}")]
-        public Product GetProduct()
+        public Product GetProduct([FromServices] ILogger<ProductsController> logger)
         {
-            return new Product()
-            {
-                ProductId = 1,
-                Name = "Test Product"
-            };
+            logger.LogDebug("GetProduct Action Invoked");
+            return context.Products.FirstOrDefault();
         }
     }
 }

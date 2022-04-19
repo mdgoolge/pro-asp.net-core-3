12b884ea2e2bb1c245391064c81195c00921886a
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-04-19 14:17:21 +0800

    Understanding Content Formatting

---
+++ CHAPTER 20 Advanced Web Service Features/WebApp/Controllers/ContentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WebApp.Models;
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ContentController : ControllerBase
+    {
+        private DataContext context;
+        public ContentController(DataContext dataContext)
+        {
+            context = dataContext;
+        }
+        [HttpGet("string")]
+        public string GetString() => "This is a string response";
+        [HttpGet("object")]
+        public async Task<Product> GetObject()
+        {
+            return await context.Products.FirstAsync();
+        }
+    }
+}

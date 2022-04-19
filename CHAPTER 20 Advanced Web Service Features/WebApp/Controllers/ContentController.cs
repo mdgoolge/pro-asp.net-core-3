0ca852e4d3355cfc09627741d0d93a189199cca0
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-04-19 15:05:12 +0800

    Specifying an Action Result Format

--- CHAPTER 20 Advanced Web Service Features/WebApp/Controllers/ContentController.cs
+++ CHAPTER 20 Advanced Web Service Features/WebApp/Controllers/ContentController.cs
@@ -16,6 +16,7 @@ namespace WebApp.Controllers
         [HttpGet("string")]
         public string GetString() => "This is a string response";
         [HttpGet("object")]
+        [Produces("application/json")]
         public async Task<ProductBindingTarget> GetObject()
         {
             Product p = await context.Products.FirstAsync();

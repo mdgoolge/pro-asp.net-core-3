8cd77926fa7bce137f482d1c38abb282ee233a26
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-04-20 14:12:12 +0800

    Preparing for This Chapter

---
+++ CHAPTER 22 Using Controllers with Views, Part II/WebApp/Startup.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
+namespace WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+        public IConfiguration Configuration { get; set; }
+
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(opts => {
+                opts.UseSqlServer(Configuration[
+                "ConnectionStrings:ProductConnection"]);
+                opts.EnableSensitiveDataLogging(true);
+            });
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+            services.AddDistributedMemoryCache();
+            services.AddSession(options => {
+                options.Cookie.IsEssential = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, DataContext context)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+                endpoints.MapDefaultControllerRoute();
+            });
+
+            SeedData.SeedDatabase(context);
+
+        }
+    }
+}

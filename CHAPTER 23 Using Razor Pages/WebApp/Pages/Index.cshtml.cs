8c06b561b9bb8a968696fbab359a935715b9b081
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-04-21 14:37:13 +0800

    Understanding the Page Model Class Using a Code-Behind Class File

--- CHAPTER 23 Using Razor Pages/WebApp/Pages/Index.cshtml.cs
+++ CHAPTER 23 Using Razor Pages/WebApp/Pages/Index.cshtml.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using WebApp.Models;
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private DataContext context;
+        public Product Product { get; set; }
+        public IndexModel(DataContext ctx)
         {
+            context = ctx;
+        }
+        public async Task OnGetAsync(long id = 1)
+        {
+            Product = await context.Products.FindAsync(id);
         }
     }
 }

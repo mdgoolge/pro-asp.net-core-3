120eaba3bb25a7d7d6efd8e6bc82a7214f3fdd2e
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-04-21 14:47:29 +0800

    Using an Action Result

--- CHAPTER 23 Using Razor Pages/WebApp/Pages/Index.cshtml.cs
+++ CHAPTER 23 Using Razor Pages/WebApp/Pages/Index.cshtml.cs
@@ -24,6 +24,10 @@ namespace WebApp.Pages
         public async Task<IActionResult> OnGetAsync(long id = 1)
         {
             Product = await context.Products.FindAsync(id);
+            if (Product == null)
+            {
+                return RedirectToPage("NotFound");
+            }
             return Page();
         }
     }

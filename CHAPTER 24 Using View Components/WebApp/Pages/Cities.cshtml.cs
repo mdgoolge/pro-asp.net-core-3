2a65898e54ffab12cfb473759887d58aa491e21f
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-04-25 14:37:20 +0800

    Creating Asynchronous View Components

--- CHAPTER 24 Using View Components/WebApp/Pages/Cities.cshtml.cs
+++ CHAPTER 24 Using View Components/WebApp/Pages/Cities.cshtml.cs
@@ -4,13 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using WebApp.Models;
 namespace WebApp.Pages
 {
+    [ViewComponent(Name = "CitiesPageHybrid")]
     public class CitiesModel : PageModel
     {
-        public void OnGet()
+        public CitiesModel(CitiesData cdata)
         {
+            Data = cdata;
+        }
+        public CitiesData Data { get; set; }
+        [ViewComponentContext]
+        public ViewComponentContext Context { get; set; }
+        public IViewComponentResult Invoke()
+        {
+            return new ViewViewComponentResult()
+            {
+                ViewData = new ViewDataDictionary<CityViewModel>(
+            Context.ViewData,
+            new CityViewModel
+            {
+                Cities = Data.Cities.Count(),
+                Population = Data.Cities.Sum(c => c.Population)
+            })
+            };
         }
     }
 }

665329471a05be035b57b921a5a844494ac667c0
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-04-25 11:00:28 +0800

    Returning HTML Fragments

--- CHAPTER 24 Using View Components/WebApp/Components/CitySummary.cs
+++ CHAPTER 24 Using View Components/WebApp/Components/CitySummary.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using WebApp.Models;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Html;
 namespace WebApp.Components
 {
     public class CitySummary : ViewComponent
@@ -15,13 +17,22 @@ namespace WebApp.Components
         //    return $"{data.Cities.Count()} cities, "
         //    + $"{data.Cities.Sum(c => c.Population)} people";
         //}
+        //public IViewComponentResult Invoke()
+        //{
+        //    return View(new CityViewModel
+        //    {
+        //        Cities = data.Cities.Count(),
+        //        Population = data.Cities.Sum(c => c.Population)
+        //    });
+        //}
+        //public IViewComponentResult Invoke()
+        //{
+        //    return Content("This is a <h3><i>string</i></h3>");
+        //}
         public IViewComponentResult Invoke()
         {
-            return View(new CityViewModel
-            {
-                Cities = data.Cities.Count(),
-                Population = data.Cities.Sum(c => c.Population)
-            });
+            return new HtmlContentViewComponentResult(
+            new HtmlString("This is a <h3><i>string</i></h3>"));
         }
     }
 }

8d5cba403b335ee87258fe44ce413059c03a5c4d
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-04-26 15:03:24 +0800

    Expanding Tag Helper Component Element Selection Summary

--- CHAPTER 25 Using Tag Helpers/WebApp/Startup.cs
+++ CHAPTER 25 Using Tag Helpers/WebApp/Startup.cs
@@ -37,7 +37,7 @@ namespace WebApp
             services.AddSingleton<CitiesData>();
 
             services.AddTransient<ITagHelperComponent, TimeTagHelperComponent>();
-
+            services.AddTransient<ITagHelperComponent, TableFooterTagHelperComponent>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

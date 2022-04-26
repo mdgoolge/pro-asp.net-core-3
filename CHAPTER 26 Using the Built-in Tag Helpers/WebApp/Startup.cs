c998bd336b4b834c5ef95eaeda5b60fa0b66cdf4
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-04-26 15:12:46 +0800

    Preparing for This Chapter

---
+++ CHAPTER 26 Using the Built-in Tag Helpers/WebApp/Startup.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using WebApp.TagHelpers;
+namespace WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+        public IConfiguration Configuration { get; set; }
+
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(opts =>
+            {
+                opts.UseSqlServer(Configuration[
+                "ConnectionStrings:ProductConnection"]);
+                opts.EnableSensitiveDataLogging(true);
+            });
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+            services.AddSingleton<CitiesData>();
+
+            services.AddTransient<ITagHelperComponent, TimeTagHelperComponent>();
+            services.AddTransient<ITagHelperComponent, TableFooterTagHelperComponent>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, DataContext context)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
+            });
+
+            SeedData.SeedDatabase(context);
+
+        }
+    }
+}

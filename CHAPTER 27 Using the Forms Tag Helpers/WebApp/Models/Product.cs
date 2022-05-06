e780139a6bc2acf28cb92143793ff3db80cb29b9
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-05-06 13:48:51 +0800

    Applying Formatting via the Model Class

--- CHAPTER 27 Using the Forms Tag Helpers/WebApp/Models/Product.cs
+++ CHAPTER 27 Using the Forms Tag Helpers/WebApp/Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 namespace WebApp.Models
 {
     public class Product
@@ -6,6 +7,7 @@ namespace WebApp.Models
         public long ProductId { get; set; }
         public string Name { get; set; }
         [Column(TypeName = "decimal(8, 2)")]
+        [DisplayFormat(DataFormatString = "{0:c2}", ApplyFormatInEditMode = true)]
         public decimal Price { get; set; }
         public long CategoryId { get; set; }
         public Category Category { get; set; }

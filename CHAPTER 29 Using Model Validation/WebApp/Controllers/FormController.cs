f050ba36a1f8e4ed447b68e410b36b9982ee6dfa
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-05-10 13:33:36 +0800

    Changing Elements in the Form.cshtml File in the Views/Form Folder Replace the contents of the FormController.cs Dropping the Database

--- CHAPTER 29 Using Model Validation/WebApp/Controllers/FormController.cs
+++ CHAPTER 29 Using Model Validation/WebApp/Controllers/FormController.cs
@@ -14,20 +14,18 @@ namespace WebApp.Controllers
         {
             context = dbContext;
         }
-        public async Task<IActionResult> Index([FromQuery]long? id)
+        public async Task<IActionResult> Index(long? id)
         {
-            ViewBag.Categories
-   = new SelectList(context.Categories, "CategoryId", "Name");
-            return View("Form", await context.Products.Include(p => p.Category)
-                        .Include(p => p.Supplier).FirstOrDefaultAsync(p => p.ProductId == id
-                            ||p.ProductId==id));
+            return View("Form", await context.Products
+                .FirstOrDefaultAsync(p => id == null || p.ProductId==id));
         }
         [HttpPost]
-        public IActionResult SubmitForm([Bind("Name","Category" )]  Product product)
+        public IActionResult SubmitForm( Product product)
         {
             TempData["name"] = product.Name;
             TempData["price"] = product.Price.ToString();
-            TempData["category name"] = product.Category.Name;
+            TempData["categoryId"] = product.CategoryId.ToString(); 
+            TempData["supplierId"] = product.SupplierId.ToString();
 
             return RedirectToAction(nameof(Results));
         }
@@ -35,19 +33,7 @@ namespace WebApp.Controllers
         {
             return View(TempData);
         }
-        //public string Header([FromHeader] string accept)
-        //{
-        //    return $"Header: {accept}";
-        //}
-        public string Header([FromHeader(Name = "Accept-Language")] string accept)
-        {
-            return $"Header: {accept}";
-        }
-        [HttpPost]
-        [IgnoreAntiforgeryToken]
-        public Product Body([FromBody] Product model)
-        {
-            return model;
-        }
+      
+       
     }
 }

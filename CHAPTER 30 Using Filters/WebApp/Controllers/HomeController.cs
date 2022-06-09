ebb75e7be389285d901eed9edabd0800ca67a022
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-06-09 11:04:04 +0800

    Using Filters Selectively Enforcing HTTPS in the HomeController.cs File in the Controllers Folder

--- CHAPTER 30 Using Filters/WebApp/Controllers/HomeController.cs
+++ CHAPTER 30 Using Filters/WebApp/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            return View("Message",
-            "This is the Index action on the Home controller");
+            if (Request.IsHttps)
+            {
+                return View("Message",
+                          "This is the Index action on the Home controller");
+            }
+            else
+            {
+                return new StatusCodeResult(StatusCodes.Status403Forbidden);
+            }
+
         }
     }
 }

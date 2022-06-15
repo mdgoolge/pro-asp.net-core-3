729e72144d52e9a2b8c95196c7506c1c41da8326
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-06-15 13:29:12 +0800

    Creating Filter Factories

--- CHAPTER 30 Using Filters/WebApp/Filters/GuidResponseAttribute.cs
+++ CHAPTER 30 Using Filters/WebApp/Filters/GuidResponseAttribute.cs
@@ -5,14 +5,24 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 namespace WebApp.Filters
 {
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class,
     AllowMultiple = true)]
-    public class GuidResponseAttribute : Attribute, IAsyncAlwaysRunResultFilter
+    public class GuidResponseAttribute : Attribute, IAsyncAlwaysRunResultFilter, IFilterFactory
     {
         private int counter = 0;
         private string guid = Guid.NewGuid().ToString();
+
+        public bool IsReusable => false;
+
+        public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
+        {
+            return ActivatorUtilities
+            .GetServiceOrCreateInstance<GuidResponseAttribute>(serviceProvider);
+        }
+
         public async Task OnResultExecutionAsync(ResultExecutingContext context,
         ResultExecutionDelegate next)
         {

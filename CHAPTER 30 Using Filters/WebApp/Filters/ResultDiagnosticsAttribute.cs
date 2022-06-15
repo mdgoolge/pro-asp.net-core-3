777d7518f97bf541637d2903e632f195d9f9c536
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-06-15 10:26:18 +0800

    Implementing a Result Filter Using the Attribute Base Class

--- CHAPTER 30 Using Filters/WebApp/Filters/ResultDiagnosticsAttribute.cs
+++ CHAPTER 30 Using Filters/WebApp/Filters/ResultDiagnosticsAttribute.cs
@@ -9,10 +9,10 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 namespace WebApp.Filters
 {
-    public class ResultDiagnosticsAttribute : Attribute, IAsyncResultFilter
+    public class ResultDiagnosticsAttribute : ResultFilterAttribute
     {
-        public async Task OnResultExecutionAsync(
-        ResultExecutingContext context, ResultExecutionDelegate next)
+        public override async Task OnResultExecutionAsync(
+ResultExecutingContext context, ResultExecutionDelegate next)
         {
             if (context.HttpContext.Request.Query.ContainsKey("diag"))
             {

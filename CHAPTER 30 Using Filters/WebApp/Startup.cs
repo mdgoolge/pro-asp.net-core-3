7230e93e8f24f9540a0488b0309ef7e8244ce40d
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-06-15 13:32:10 +0800

    Using Dependency Injection Scopes to Manage Filter Lifecycles

--- CHAPTER 30 Using Filters/WebApp/Startup.cs
+++ CHAPTER 30 Using Filters/WebApp/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Filters;
 namespace WebApp
 {
     public class Startup
@@ -44,7 +45,9 @@ namespace WebApp
             }); 
             
             services.Configure<MvcOptions>(opts => opts.ModelBindingMessageProvider
- .SetValueMustNotBeNullAccessor(value => "Please enter a value"));
+ .SetValueMustNotBeNullAccessor(value => "Please enter a value")); 
+            
+            services.AddScoped<GuidResponseAttribute>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

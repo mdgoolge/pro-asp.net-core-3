bbaf13ea1f8f2c3ab9199c4c1307a24a28c77f48
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-06-15 09:59:25 +0800

    Using the Controller Filter Methods

--- CHAPTER 30 Using Filters/WebApp/Controllers/HomeController.cs
+++ CHAPTER 30 Using Filters/WebApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using WebApp.Filters;
+using Microsoft.AspNetCore.Mvc.Filters;
 namespace WebApp.Controllers
 {
     [HttpsOnly]
@@ -18,10 +19,17 @@ namespace WebApp.Controllers
             return View("Message",
             "This is the Secure action on the Home controller");
         }
-        [ChangeArg]
+        //[ChangeArg]
         public IActionResult Messages(string message1, string message2 = "None")
         {
             return View("Message", $"{message1}, {message2}");
+        }
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ActionArguments.ContainsKey("message1"))
+            {
+                context.ActionArguments["message1"] = "New message";
+            }
         }
     }
 }

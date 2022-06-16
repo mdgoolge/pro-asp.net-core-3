24c3fb1020e9b4866784bf2eb524a58a834463a4
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-06-16 12:58:33 +0800

    Reading Data

--- CHAPTER 31 Creating Form Applications/WebApp/Controllers/HomeController.cs
+++ CHAPTER 31 Creating Form Applications/WebApp/Controllers/HomeController.cs
@@ -20,5 +20,27 @@ namespace WebApp.Controllers
             return View(context.Products.
             Include(p => p.Category).Include(p => p.Supplier));
         }
+        public async Task<IActionResult> Details(long id)
+        {
+            Product p = await context.Products.
+            Include(p => p.Category).Include(p => p.Supplier)
+            .FirstOrDefaultAsync(p => p.ProductId == id);
+            ProductViewModel model = ViewModelFactory.Details(p);
+            return View("ProductEditor", model);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ CHAPTER 31 Creating Form Applications/WebApp/Models/ViewModelFactory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace WebApp.Models
+{
+    public static class ViewModelFactory
+    {
+        public static ProductViewModel Details(Product p)
+        {
+            return new ProductViewModel
+            {
+                Product = p,
+                Action = "Details",
+                ReadOnly = true,
+                Theme = "info",
+                ShowAction = false,
+                Categories = p == null ? Enumerable.Empty<Category>()
+            : new List<Category> { p.Category },
+                Suppliers = p == null ? Enumerable.Empty<Supplier>()
+            : new List<Supplier> { p.Supplier },
+            };
+        }
+    }
+}

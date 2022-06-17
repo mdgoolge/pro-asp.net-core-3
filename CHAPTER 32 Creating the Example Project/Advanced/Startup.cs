7a05a42d495141e1c1505248c6202f735c09c0e9
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-06-17 10:17:17 +0800

    Configuring Entity Framework Core Services and Middleware

--- CHAPTER 32 Creating the Example Project/Advanced/Startup.cs
+++ CHAPTER 32 Creating the Example Project/Advanced/Startup.cs
@@ -7,34 +7,43 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Advanced.Models;
 
 namespace Advanced
 {
     public class Startup
     {
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+        public IConfiguration Configuration { get; set; }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(opts => {
+                opts.UseSqlServer(Configuration[
+                "ConnectionStrings:PeopleConnection"]);
+                opts.EnableSensitiveDataLogging(true);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, DataContext context)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            app.UseDeveloperExceptionPage();
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
+            app.UseEndpoints(endpoints => {
+                endpoints.MapGet("/", async context => {
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+            SeedData.SeedDatabase(context);
         }
     }
 }

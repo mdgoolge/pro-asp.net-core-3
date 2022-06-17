db78ce15943c939ddb331f09928fee805981d4dc
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-06-17 10:22:25 +0800

    Configuring the Services and Middleware

--- CHAPTER 32 Creating the Example Project/Advanced/Startup.cs
+++ CHAPTER 32 Creating the Example Project/Advanced/Startup.cs
@@ -31,17 +31,21 @@ namespace Advanced
                 "ConnectionStrings:PeopleConnection"]);
                 opts.EnableSensitiveDataLogging(true);
             });
+
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, DataContext context)
         {
             app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
             app.UseRouting();
             app.UseEndpoints(endpoints => {
-                endpoints.MapGet("/", async context => {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("controllers","controllers/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
             });
             SeedData.SeedDatabase(context);
         }

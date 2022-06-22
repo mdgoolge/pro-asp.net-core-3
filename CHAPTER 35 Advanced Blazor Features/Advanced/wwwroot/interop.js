4974a626bfb9fae95521a9960e7b98e51b1d2de1
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-06-22 15:38:30 +0800

    Invoking a Component Method from JavaScript

--- CHAPTER 35 Advanced Blazor Features/Advanced/wwwroot/interop.js
+++ CHAPTER 35 Advanced Blazor Features/Advanced/wwwroot/interop.js
@@ -8,3 +8,12 @@
         row.append(cell);
     }
 }
+
+function createToggleButton() {
+    let sibling = document.querySelector("button:last-of-type");
+    let button = document.createElement("button");
+    button.classList.add("btn", "btn-secondary", "btn-block");
+    button.innerText = "JS Toggle";
+    sibling.parentNode.insertBefore(button, sibling.nextSibling);
+    button.onclick = () => DotNet.invokeMethodAsync("Advanced", "ToggleEnabled");
+}

85a39bc579b7775209b977ac06f93e55c371f589
gengtiwin@hotmail.com <gengtiwin@hotmail.com>
2022-07-18 17:15:11 +0800

    Enabling the Authorization Middleware

--- CHAPTER 39 Applying ASP.NET Core Identity/Advanced/Startup.cs
+++ CHAPTER 39 Applying ASP.NET Core Identity/Advanced/Startup.cs
@@ -70,6 +70,7 @@ namespace Advanced
             app.UseRouting();
 
             app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
